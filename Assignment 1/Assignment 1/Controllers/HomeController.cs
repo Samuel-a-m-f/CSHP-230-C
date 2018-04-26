451189b36ebcba5f97db418fe3414da4dc8036d0
Samuel.a.m.f <Samuel.a.m.f@gmail.com>
2018-04-25 23:06:46 -0700

    Added Assignment 1

---
+++ Assignment 1/Assignment 1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BirthdayForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BirthdayForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
