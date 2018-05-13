7746b41ebdfceef0688cc2053492aa3e88ba8322
Samuel.a.m.f <Samuel.a.m.f@gmail.com>
2018-05-13 14:19:35 -0700

    Added Assignment 2

---
+++ WebStore/src/LearningCenter.Repository/CategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace LearningCenter.Repository
+{
+    public interface ICategoryRepository
+    {
+        CategoryModel[] Categories { get; }
+        CategoryModel Category(int categoryId);
+    }
+
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CategoryRepository : ICategoryRepository
+    {
+        public CategoryModel[] Categories
+        {
+            get
+            {
+                return DatabaseAccessor.Instance.Classes
+                                               .Select(t => new CategoryModel { Id = t.ClassId, Name = t.ClassName })
+                                               .ToArray();
+            }
+        }
+
+        public CategoryModel Category(int categoryId)
+        {
+            var category = DatabaseAccessor.Instance.Classes
+                                                   .Where(t => t.ClassId == categoryId)
+                                                   .Select(t => new CategoryModel { Id = t.ClassId, Name = t.ClassName })
+                                                   .First();
+            return category;
+        }
+    }
+}
---
+++ WebStore/src/LearningCenter.Repository/DatabaseAccessor.cs
@@ -0,0 +1,23 @@
+﻿using LearningCenter.ProductDatabase;
+
+namespace LearningCenter.Repository
+{
+    public class DatabaseAccessor
+    {
+        private static readonly Entities entities;
+
+        static DatabaseAccessor()
+        {
+            entities = new Entities();
+            entities.Database.Connection.Open();
+        }
+
+        public static Entities Instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
